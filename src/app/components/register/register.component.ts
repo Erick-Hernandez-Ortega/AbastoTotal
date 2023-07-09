3e86fba0c923f6ce7a40ea4e36909156172d010a
Erick Ortega <erickramon47@live.com.mx>
2023-07-08 20:52:52 -0600

    Se integra una version temprana de registro de usuario

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,18 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  loading : boolean = true
-  constructor() { }
+  loading: boolean = true;
+  name: string = '';
+  last_names: string = '';
+  email: string = '';
+  phone: string = '';
+  password: string = '';
+
+  constructor() {}
 
   ngOnInit() {
     setTimeout(() => {
-      this.loading = false
+      this.loading = false;
     }, 1000);
   }
 
+  registrarUsuario(): void {
+    const auth = getAuth();
+
+    createUserWithEmailAndPassword(auth, this.email, this.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert("Se registro el usuario")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+
+    
+  }
 }
