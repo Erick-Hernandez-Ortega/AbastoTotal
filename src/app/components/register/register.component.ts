fb9107af73ed52c5b04a8d72f220ced09919bebb
Erick Ortega <erickramon47@live.com.mx>
2023-07-19 21:24:07 -0600

    Registrar funciona

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -14,10 +14,11 @@ export class RegisterComponent implements OnInit {
   email: string = '';
   phone: string = '';
   password: string = '';
-  messageModal: string = "";
-  modalError: boolean = false
+  password2: string = '';
+  messageModal: string = '';
+  modalError: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     setTimeout(() => {
@@ -28,36 +29,54 @@ export class RegisterComponent implements OnInit {
   registrarUsuario(): void {
     const auth = getAuth();
 
-    if (!this.name.trim().match(/^[a-zA-Z\s']+$/) || !this.last_names.trim().match(/^[a-zA-Z\s']+$/)) {
-      this.messageModal = "Error en el nombre de usuario y/o apellido"
-      this.modalError = true
-      this.showModal = !this.showModal
-    }
-    else {
+    if (
+      !this.name.trim().match(/^[a-zA-Z\s']+$/) ||
+      !this.last_names.trim().match(/^[a-zA-Z\s']+$/)
+    ) {
+      this.lauchModalError('Error en el nombre de usuario y/o apellido');
+    } else if (
+      !this.email
+        .trim()
+        .match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    ) {
+      this.lauchModalError('Error en el correo electronico');
+    } else if (!this.phone.trim().match(/^\d{10}$/)) {
+      this.lauchModalError('Error en el número telefonico');
+    } else if (this.password.trim() !== this.password2.trim()) {
+      this.lauchModalError('Las contraseñas no coinciden');
+    } else if (!this.password.trim().match(/^(?=.*[A-Z]).{8,}$/)) {
+      this.lauchModalError(
+        'La contraseña debe ser minimo 8 caracteres y tener una mayuscula'
+      );
+    } else {
       createUserWithEmailAndPassword(auth, this.email, this.password)
         .then((userCredential) => {
-          // Signed in
           const user = userCredential.user;
-          this.messageModal = "El usuario fue registrado con exito"
-          this.modalError = false
-          this.showModal = !this.showModal
-          this.cleanInputs()
-          // ...
+          this.messageModal = 'El usuario fue registrado con exito';
+          this.modalError = false;
+          this.showModal = !this.showModal;
+          this.cleanInputs();
         })
         .catch((error) => {
-          const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
-          alert(`Error: ${errorMessage} ${errorCode}`)
+
+          this.lauchModalError(errorMessage);
         });
     }
   }
 
+  lauchModalError(message: string): void {
+    this.messageModal = message;
+    this.modalError = true;
+    this.showModal = true;
+  }
+
   cleanInputs(): void {
-    this.name = ""
-    this.last_names = ""
-    this.email = ""
-    this.password = ""
-    this.phone = ""
+    this.name = '';
+    this.last_names = '';
+    this.email = '';
+    this.password = '';
+    this.phone = '';
+    this.password2 = '';
   }
 }
