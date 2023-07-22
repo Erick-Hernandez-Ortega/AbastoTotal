e74e9619c2b9e04dffb65138b65773f6236956a9
Erick Ortega <erickramon47@live.com.mx>
2023-07-21 20:36:57 -0600

    Se agrega codigo para iniciar sesión

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email : string = ""
+  password : string = ""
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  signIn() : void {
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, this.email, this.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("Seseion iniciada: " + user.email);
+    
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
   }
 
 }
