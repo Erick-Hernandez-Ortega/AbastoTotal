e321e31c3be33fdaa515e8306e4f91f08cc0c9dd
Erick Ortega <erickramon47@live.com.mx>
2023-07-25 20:06:16 -0600

    Se pone formato al codigo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  email : string = ""
-  password : string = ""
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  signIn() : void {
+  signIn(): void {
     const auth = getAuth();
 
     signInWithEmailAndPassword(auth, this.email, this.password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log("Seseion iniciada: " + user.email);
-    
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-  }
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('Seseion iniciada: ' + user.email);
 
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 }
