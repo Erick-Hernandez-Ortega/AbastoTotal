97ba68cd26c26b466d9068a561c364d70ede5410
Erick Ortega <erickramon47@live.com.mx>
2023-07-30 17:42:00 -0600

    Se agrega la funcion de cerrar sesion

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { getAuth, signOut } from 'firebase/auth';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  contenido: string = '';
+  showModal: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  signOut() {
+    const auth = getAuth();
+
+    signOut(auth)
+      .then(() => {
+        alert('Se ha cerrado la sesión correctamente');
+
+        this.router.navigate(['']);
+      })
+      .catch((error) => {
+        // An error happened.
+      });
   }
-
 }
