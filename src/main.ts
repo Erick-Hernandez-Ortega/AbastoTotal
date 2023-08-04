be3e3b1418feaad36b4d3c33f7c78fed7cd8b730
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-03 19:49:55 -0600

    Se inicializa y se exporta la variable para BD Firestore

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { firebaseConfig } from './environments/environments';
-
+import { getFirestore } from "firebase/firestore";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
@@ -11,3 +11,6 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
