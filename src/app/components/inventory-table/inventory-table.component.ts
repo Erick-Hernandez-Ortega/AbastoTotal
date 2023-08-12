8afd79dffee26c11af189c103fb43d79d0e82ead
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-11 18:43:50 -0600

    Ya se pueden actualziar las productos

--- src/app/components/inventory-table/inventory-table.component.ts
+++ src/app/components/inventory-table/inventory-table.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { collection, doc, onSnapshot } from 'firebase/firestore';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from 'firebase/firestore';
 import { db } from 'src/main';
 
 @Component({
@@ -9,6 +15,12 @@ import { db } from 'src/main';
 })
 export class InventoryTableComponent implements OnInit {
   productos: any[] = [];
+  idActualizar: string = '';
+  nombreProductoActualizar: string = '';
+  existenciasProductoActualizar: number = 0;
+  costoProductoActualizar: number = 0;
+  precioProductoActualizar: number = 0;
+  @ViewChild('closeButton', { static: true }) closeButton!: ElementRef;
 
   constructor() {}
 
@@ -21,8 +33,34 @@ export class InventoryTableComponent implements OnInit {
     });
   }
 
-  updateProduct(id: string): void {
-    console.log(id);
+  async showProduct(id: string) {
+    this.idActualizar = id;
+    const docRef = doc(db, 'productos', id);
+    const docSnap = await getDoc(docRef);
+
+    const producto = docSnap.data();
+    this.nombreProductoActualizar = producto!['nombre'];
+    this.existenciasProductoActualizar = producto!['existencias'];
+    this.costoProductoActualizar = producto!['costo'];
+    this.precioProductoActualizar = producto!['precio'];
+  }
+
+  async updateProduct() {
+    const docRef = doc(db, 'productos', this.idActualizar);
+    const margen =
+      ((this.precioProductoActualizar - this.costoProductoActualizar) /
+        this.costoProductoActualizar) *
+      100;
+
+    await updateDoc(docRef, {
+      nombre: this.nombreProductoActualizar,
+      costo: this.costoProductoActualizar,
+      existencias: this.existenciasProductoActualizar,
+      precio: this.precioProductoActualizar,
+      margen: margen.toFixed(2),
+    });
+    this.closeButton.nativeElement.click();
+    alert('Se ha actualizado con exito!');
   }
 
   deleteProduct(id: string): void {
