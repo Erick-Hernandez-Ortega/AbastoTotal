bb5c1742984f16794a8e3a1bf3fc2c6c0a51646e
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-14 20:45:49 -0600

    Se muestra los proveedores

--- src/app/components/table-suppliers/table-suppliers.component.ts
+++ src/app/components/table-suppliers/table-suppliers.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { db } from 'src/main';
 
 @Component({
   selector: 'app-table-suppliers',
-  templateUrl: './table-suppliers.component.html'
+  templateUrl: './table-suppliers.component.html',
+  styleUrls: ['./table-suppliers.component.css'],
 })
 export class TableSuppliersComponent implements OnInit {
+  suppliers: any[] = [];
+  limit: number = 10;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.loadDataSuppliers();
   }
 
+  async loadDataSuppliers() {
+    const q = query(
+      collection(db, 'proveedores'),
+      orderBy('nombre'),
+      limit(this.limit)
+    );
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      this.suppliers = [];
+      querySnapshot.forEach((doc) => {
+        this.suppliers.push({ id: doc.id, ...doc.data() });
+      });
+      console.log(this.suppliers);
+    });
+  }
 }
