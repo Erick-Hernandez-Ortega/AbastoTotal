034c838cef0a65edd35a02bd7f06d3773bce5a42
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-14 19:20:04 -0600

    Se pone el componende header ademas del sidebar al componente: proveedores

--- src/app/components/suppliers/suppliers.component.ts
+++ src/app/components/suppliers/suppliers.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, signOut } from 'firebase/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-suppliers',
   templateUrl: './suppliers.component.html',
-  styleUrls: ['./suppliers.component.css']
+  styleUrls: ['./suppliers.component.css'],
 })
 export class SuppliersComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  signOut() {
+    const auth = getAuth();
+
+    signOut(auth)
+      .then(() => {
+        alert('Se ha cerrado la sesión correctamente');
+
+        this.router.navigate(['']);
+      })
+      .catch((error) => {
+        alert(`Hubo un error: ${error}`);
+      });
   }
-
 }
