ab49e88f56bbc41e46a65db6ac1a677f5135f3e3
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-15 21:48:18 -0600

    Se trabaja en agregar un proveedor: se pone verificaciones

--- src/app/components/suppliers/suppliers-buttons/suppliers-buttons.component.ts
+++ src/app/components/suppliers/suppliers-buttons/suppliers-buttons.component.ts
@@ -2,13 +2,50 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-suppliers-buttons',
-  templateUrl: './suppliers-buttons.component.html'
+  templateUrl: './suppliers-buttons.component.html',
 })
 export class SuppliersButtonsComponent implements OnInit {
+  clave: string = '';
+  nombre: string = '';
+  nombreRazon: string = '';
+  rfc: string = '';
+  direccion: string = '';
+  titular: string = '';
+  telefono: string = '';
+  email: string = '';
+  banco: string = '';
+  nCuenta: string = '';
+  clabe: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async addSuppliers() {
+    if (this.checkFields()) {
+      alert("Un campo esta vacio verifique y llenelo de nuevo")
+    } else {
+      console.log(this.banco);
+    }
   }
 
+  checkFields(): boolean {
+    if (
+      this.clave === '' ||
+      this.nombre === '' ||
+      this.nombreRazon === '' ||
+      this.rfc === '' ||
+      this.direccion === '' ||
+      this.titular === '' ||
+      this.telefono === '' ||
+      this.email === '' ||
+      this.banco === '' ||
+      this.nCuenta === '' ||
+      this.clabe === ''
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
