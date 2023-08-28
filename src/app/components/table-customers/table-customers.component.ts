06728a020ea8b7081ada533f93c496ca159e33c6
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-08-27 21:25:40 -0600

    Se visualiza los clientes guardados en la bd

--- src/app/components/table-customers/table-customers.component.ts
+++ src/app/components/table-customers/table-customers.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from 'src/main';
 
 @Component({
   selector: 'app-table-customers',
   templateUrl: './table-customers.component.html',
-  styleUrls: ['./table-customers.component.css']
+  styleUrls: ['./table-customers.component.css'],
 })
 export class TableCustomersComponent implements OnInit {
+  limit : number = 5;
+  customers : any[] = []
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.loadDataCustomers();
   }
 
+  async loadDataCustomers() {
+    const q = query(
+      collection(db, 'clientes'),
+      orderBy('clave'),
+      limit(this.limit)
+    );
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      this.customers = [];
+      querySnapshot.forEach((doc) => {
+        this.customers.push({ id: doc.id, ...doc.data() });
+      }); 
+    });
+  }
 }
