61346affb263f9f45557d2e7691da88257add4c2
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-09-15 21:03:32 -0600

    Se agrega validacion de los inputs, ademas de agregar ngModel a los mismos

--- src/app/components/customers/customers-buttons/customers-buttons.component.ts
+++ src/app/components/customers/customers-buttons/customers-buttons.component.ts
@@ -2,13 +2,53 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customers-buttons',
-  templateUrl: './customers-buttons.component.html'
+  templateUrl: './customers-buttons.component.html',
 })
 export class CustomersButtonsComponent implements OnInit {
+  clave: string = '';
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
+  direccion: string = '';
+  isPagoEfectivo: boolean = true;
+  banco: string = '';
+  nCuenta: string = '';
+  clabe: string = '';
+  isActivo: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addCustomer(): void {}
+
+  cleanInputs(): void {
+    this.clabe = '';
+    this.nombre = '';
+    this.email = '';
+    this.telefono = '';
+    this.direccion = '';
+    this.banco = '';
+    this.nCuenta = '';
   }
 
+  checkInputs(): void {
+    if (this.isPagoEfectivo) {
+      if (this.clave.length === 3) {
+        this.isActivo = false;
+      } else {
+        this.isActivo = true;
+      }
+    } else {
+      if (
+        this.clave.length === 3 &&
+        this.clabe.length === 18 &&
+        this.nCuenta.length === 10
+      ) {
+        this.isActivo = false;
+      } else {
+        this.isActivo = true;
+      }
+    }
+  }
 }
